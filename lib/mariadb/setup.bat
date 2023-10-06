1c0e48e272600ee0d051585dd5e9366702dd281f
Aymeric Anger <daedelus.git@gmail.com>
2023-10-07 01:18:47 +0200

    🔧 Mariadb Setup + some deps

---
+++ lib/mariadb/setup.bat
@@ -0,0 +1,9 @@
+@echo off
+if not exist "%~dp0databases\mysql" (
+    echo Installing databases...
+    pushd %~dp0
+        bin\mariadb-install-db.exe -R -D -p=toor -c .\my-default.ini
+    popd
+    exit /b
+)
+

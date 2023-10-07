ebc6f03c46c3ccfbc7670b8420d833179edb8e44
Aymeric Anger <daedelus.git@gmail.com>
2023-10-07 05:01:58 +0200

    ✨ Startup Scripts

--- lib/sdk/scripts/start-nginx.bat
+++ lib/sdk/scripts/start-nginx.bat
@@ -5,7 +5,7 @@ call "%~dp0stop-nginx.bat" > NUL 2>&1
 call "%~dp0..\loadenv.bat"
 
 pushd "%nginx%"
-    echo Starting Nginx...
+    echo Starting Nginx on port 80...
     "%SDK%daemonize.exe" .\nginx.exe
 popd
 

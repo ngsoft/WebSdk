844f6bde93919c7157d5d693d6d454bdeb6a84c6
Aymeric Anger <daedelus.git@gmail.com>
2023-10-08 00:47:36 +0200

    ✨ checks if dir exists before adding to path conditionally

--- lib/sdk/setup.bat
+++ lib/sdk/setup.bat
@@ -2,12 +2,18 @@
 
 goto main
 
-:addpath_sdk [%1 - param] (
+:addpath_sdk [%1 - path] (
     call :addpath "%WEB_SDK%%~1"
     exit /b
 )
 
-:addpath [%1 - param] (
+:addpath [%1 - path, %2 - check] (
+
+    if not exist "%~1" (
+        if "%~2" neq "false" (
+            exit /b
+        )
+    )
     reg query HKCU\Environment /v PATH | findstr /C:"%~1" > NUL 2>&1
     if ERRORLEVEL 1 (
         echo Adding %~1 to PATH
@@ -32,7 +38,7 @@ goto main
     if ERRORLEVEL 1 (
         echo Installing NVM Node Path...
         regenv.exe set -nU -x NVM_SYMLINK "%NVM_SYMLINK%"
-        call :addpath "%NVM_SYMLINK%"
+        call :addpath "%NVM_SYMLINK%" false
         set installed=true
     )
 
@@ -53,7 +59,7 @@ goto main
     pushd "%~dp0"
         call loadenv.bat
         call :install_nvm
-        for %%f in (bin lib\mariadb\bin lib\php\8.1 lib\php\7.4 lib\php\8.2 lib\php\5.6 lib\pear) do (
+        for %%f in (bin lib\mariadb\bin lib\php\8.1 lib\php\7.4 lib\php\8.2 lib\php\5.6 lib\pear lib\go\bin) do (
             call :addpath_sdk "%%f"
         )
     popd

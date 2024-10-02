00831b920b268cabc749316676f97bf0ff113b9c
Aymeric Anger <daedelus.git@gmail.com>
2024-10-02 11:58:31 +0200

    ✨ apache module php extensions compatibility

--- var/www/adminer/plugins/AdminerBootstrapSelect.php
+++ var/www/adminer/plugins/AdminerBootstrapSelect.php
@@ -4,9 +4,27 @@ class AdminerBootstrapSelect
 {
 
 
+    protected $dark;
+
+    public function __construct($dark = false)
+    {
+        $this->dark = $dark;
+    }
+
+
     public function head()
     {
-        echo include __DIR__ . '/../layout/bootstrap-select.php';
+        ?>
+        <link rel="stylesheet" <?= nonce() ?> type="text/css" href="./static/select.css">
+        <script <?= nonce() ?> type="module">
+            document.querySelectorAll('select').forEach(s => {
+                s.classList.add('form-select', 'form-select-sm')
+            });
+            <?php if($this->dark) : ?>
+            document.documentElement.setAttribute("data-bs-theme", "dark");
+            <?php endif; ?>
+        </script>
+        <?php
     }
 }
 

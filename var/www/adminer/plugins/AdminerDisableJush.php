f70f07ed99ca0e5e2e5fd963f85f6f38acb25af4
Aymeric Anger <daedelus.git@gmail.com>
2024-12-15 07:14:04 +0100

    ✨ phpinfo

--- var/www/adminer/plugins/AdminerDisableJush.php
+++ var/www/adminer/plugins/AdminerDisableJush.php
@@ -9,29 +9,29 @@ class AdminerDisableJush
 {
     public function head()
     {
-        ?>
+?>
         <script<?php echo nonce(); ?> type="text/javascript">
             (function () {
-                // theme switcher
-                addEventListener("DOMContentLoaded", () => {
-                    document.querySelectorAll('#menu .links').forEach(el => {
-                        const c = document.createElement('div');
-                        c.innerHTML = `<a href="./theme-switcher.php">Switch theme</a>`
-                        c.style.padding = `8px`;
-                        el.parentElement.insertBefore(c, el.parentElement.querySelector('h1').nextElementSibling);
-                    });
-                });
-                var origBodyLoad = bodyLoad,
-                    tags = document.getElementsByTagName('textarea');
+            // theme switcher
+            addEventListener("DOMContentLoaded", () => {
+            document.querySelectorAll('#menu .links').forEach(el => {
+            const c = document.createElement('div');
+            c.innerHTML = `<a href="./theme-switcher.php">Switch theme</a><a href="./info.php">PHP Infos</a>`
+            c.style = `padding: 8px; display:flex; column-gap:8px;justify-content: space-between;`;
+            el.parentElement.insertBefore(c, el.parentElement.querySelector('h1').nextElementSibling);
+            });
+            });
+            var origBodyLoad = bodyLoad,
+            tags = document.getElementsByTagName('textarea');
 
-                bodyLoad = function (version) {
-                    for (var i = 0; i < tags.length; i++) {
-                        tags[i].className = tags[i].className.replace(/\bjush-\S+/, '');
-                    }
-                    return origBodyLoad.apply(window, arguments);
+            bodyLoad = function (version) {
+            for (var i = 0; i < tags.length; i++) {
+                tags[i].className=tags[i].className.replace(/\bjush-\S+/, '' );
                 }
-            })();
-        </script>
+                return origBodyLoad.apply(window, arguments);
+                }
+                })();
+                </script>
         <?php
     }
 }

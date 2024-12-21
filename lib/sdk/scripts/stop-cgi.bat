17d1d573b6ec95a94e01c9ac21dc8c720cb7915f
Aymeric Anger <daedelus.git@gmail.com>
2024-12-21 18:05:31 +0100

    ✨ add opcache/jit file storage

--- lib/sdk/scripts/stop-cgi.bat
+++ lib/sdk/scripts/stop-cgi.bat
@@ -6,6 +6,13 @@ echo Stopping PHP FastCGI...
 @REM Loads Environment
 call "%~dp0..\loadenv.bat"
 
+@REM Create PHP OPCache storage
+md "%WEB_SDK%tmp\opcache" > NUL 2>&1
+md "%WEB_SDK%tmp\opcache\8.2" > NUL 2>&1
+md "%WEB_SDK%tmp\opcache\8.3" > NUL 2>&1
+md "%WEB_SDK%tmp\opcache\8.4" > NUL 2>&1
+
+
 @REM Checks UAC
 NET FILE > NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto script

7976ae60f31f168958021f5088c9f58b03f8868c
Aymeric Anger <daedelus.git@gmail.com>
2025-09-11 21:05:14 +0200

    🔥 adminer 5.3

--- var/www/adminer/config/config.php
+++ var/www/adminer/config/config.php
@@ -2,6 +2,5 @@
 
 use Adminer\Config;
 
-Config::setItem('ADMINER_VERSION', '5.4.0');
 Config::setItem('ADMINER_ACL', ['127.0', '::1']);
 Config::setItem('ADMINER_SAVEFILE', dirname(__DIR__, 4) . '/tmp/adminer.servers');

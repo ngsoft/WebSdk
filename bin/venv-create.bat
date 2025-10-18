88eca38cbaac702e414a42eeaf9d7603d2a69c6f
Aymeric Anger <daedelus.git@gmail.com>
2025-10-18 03:08:58 +0200

    ✨ venv-create cmd switches

--- bin/venv-create.bat
+++ bin/venv-create.bat
@@ -1,13 +1,47 @@
 @echo off
-setlocal
-if NOT [%~1] == [] goto main
+setlocal enabledelayedexpansion
+set add_to_path=false
+set add_scripts_to_path=false
+set help=false
+set "project="
+
+echo "%*" | findstr /C:"/?" > NUL 2>&1
+if not ERRORLEVEL 1 set help=true
+
+@REM set argCount=0
+for %%x in (%*) do (
+    @REM set /A argCount+=1
+    if [-p] == [%%~x] (
+        set add_to_path=true
+    ) else if [/p] == [%%~x] (
+        set add_to_path=true
+    ) else if [-s] == [%%~x] (
+        set add_scripts_to_path=true
+    ) else if [/s] == [%%~x] (
+    set add_scripts_to_path=true
+    ) else if [--help] == [%%~x] (
+        set help=true
+    ) else if "[!project!]" == "[]" (
+        set "project=%%~x"
+    )
+)
+
+if [true] == [%help%] goto usage
+if NOT "[%project%]" == "[]" goto main
 
 :usage
-echo venv-create [name] >& 2
+echo Create a python virtual environment project
+echo     venv-create [project_name] [options] >&2
+echo.
+echo Options: >&2
+echo     --help,/? this help >&2
+echo     -s,/s     add python scripts from project venv to path >&2
+echo     -p,/p     add scripts from project bin to path >&2
+echo.
 exit /b 1
 
 :addpath [%1 - path, %2 - check] (
-
+    if [%add_to_path%] == [false] exit /b
     if not exist "%~1" (
         if "%~2" neq "false" (
             exit /b
@@ -34,7 +68,8 @@ if exist "%project_root%" (
 
 echo create %project_root%
 mkdir "%project_root%\bin"
-call :addpath "%project_root%\bin"
+if [%add_to_path%] == [true] call :addpath "%project_root%\bin"
+if [%add_scripts_to_path%] == [true] call :addpath "%project_root%\.venv\Scripts"
 pushd "%project_root%"
     echo Updating pip
     call py.bat -m pip install --upgrade pip
@@ -44,7 +79,10 @@ pushd "%project_root%"
     pushd bin
         echo create %project_root%\bin\%project%.bat
         echo @echo off > "%project%.bat"
-        echo call %~dp0.venv\Scripts\activate.bat >> "%project%.bat"
+        echo pushd "%project_root%" >> "%project%.bat"
+        echo call .venv\Scripts\activate.bat >> "%project%.bat"
+        echo popd >> "%project%.bat"
+        echo @REM put your code there >> "%project%.bat"
     popd
     echo open code editor in %project_root%
     code .

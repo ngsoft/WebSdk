9219acdca7baac8542a6dff8de149af8ec86e7b8
Aymeric Anger <daedelus.git@gmail.com>
2025-10-30 19:13:20 +0100

    ✨ laravel cli update

--- bin/laravel.bat
+++ bin/laravel.bat
@@ -5,6 +5,13 @@ set "script_root=%~dp0..\lib\laravel"
 set "runtime=%script_root%\vendor\bin\laravel.bat"
 goto main
 
+:update
+if not exist "%runtime%" goto install
+pushd "%script_root%"
+call "%~dp0composer8.4.bat" update
+popd
+goto :eof
+
 
 :install
 md "%script_root%"
@@ -17,6 +24,7 @@ if not exist "%runtime%" goto :eof
 
 :main
 if not exist "%runtime%" goto install
+if [%~1] == [update] goto update
 if not defined php (
     call "%~dp0..\lib\sdk\loadenv.bat"
 )

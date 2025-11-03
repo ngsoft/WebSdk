0954b9dc3c4a144882dd391296dcf5e7777cd697
Aymeric Anger <daedelus.git@gmail.com>
2025-11-03 01:14:42 +0100

    🧑‍💻 add an install step to project for php-cs-fixer

--- bin/php-cs-fixer.bat
+++ bin/php-cs-fixer.bat
@@ -3,7 +3,8 @@
 setlocal
 call "%~dp0../lib/sdk/loadenv.bat"
 set "script_root=%lib%php-cs-fixer"
-set "runtime=%script_root%\vendor\bin\php-cs-fixer.bat"
+set "runtime_detect=vendor\bin\php-cs-fixer.bat"
+set "runtime=%script_root%\%runtime_detect%"
 set "config_file=.php-cs-fixer.dist.php"
 set "template_file=%etc%.php-cs-fixer.dist"
 goto main
@@ -27,9 +28,12 @@ if not exist "%runtime%" exit /b 1
 :main
 if not exist "%runtime%" call :install
 if [%~1] == [update] goto update
-if [fix] == [%~1] if exist composer.json if not exist "%config_file%" (
+
+if exist composer.json if not exist "%config_file%" if [setup] == [%~1] (
     copy "%template_file%" "%config_file%" > NUL
     echo %config_file% has been generated from template, please review it.
+    if not exist "%runtime_detect%" call "%~dp0composer.bat" --dev require friendsofphp/php-cs-fixer
+    exit /b
 )
 
 call "%runtime%" %*

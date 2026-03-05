d14bd968c8166da191bca7f9cf98f0b4c43b4519
Aymeric Anger <daedelus.git@gmail.com>
2026-03-05 22:52:59 +0100

    🔨 php-cs-fixer

--- bin/php-cs-fixer.bat
+++ bin/php-cs-fixer.bat
@@ -2,8 +2,11 @@
 
 setlocal
 call "%~dp0../lib/sdk/loadenv.bat"
+set "php=%php82%"
 set "script_root=%lib%php-cs-fixer"
-set "runtime_detect=vendor\bin\php-cs-fixer.bat"
+set "runtime_detect_dir=vendor\bin"
+set "runtime_detect=%runtime_detect_dir%\php-cs-fixer"
+set "runtime_root=%script_root%\%runtime_detect_dir%"
 set "runtime=%script_root%\%runtime_detect%"
 set "config_file=.php-cs-fixer.dist.php"
 set "template_file=%etc%.php-cs-fixer.dist"
@@ -36,5 +39,7 @@ if exist composer.json if not exist "%config_file%" if [setup] == [%~1] (
     exit /b
 )
 
-call "%runtime%" %*
-
+setlocal DISABLEDELAYEDEXPANSION
+SET BIN_TARGET=%runtime%
+SET "COMPOSER_RUNTIME_BIN_DIR=%runtime_root%"
+"%php%php.exe" "%BIN_TARGET%" %*

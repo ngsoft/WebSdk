9c850f49e1a259f55a9f6b933e9e67cfeb810395
Aymeric Anger <daedelus.git@gmail.com>
2026-08-05 00:50:15 +0200

    ✨ cursor-agent api

---
+++ bin/cursor-server.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set CURSOR_BRIDGE_FORCE=true
+set CURSOR_BRIDGE_MODE=agent
+set CURSOR_BRIDGE_USE_ACP=true
+set CURSOR_AGENT_BIN=%LOCALAPPDATA%\cursor-agent\agent.cmd
+if exist "%CURSOR_AGENT_BIN%"  (
+    if not defined CURSOR_API_KEY (
+        "%CURSOR_AGENT_BIN%" login
+    )
+    npx cursor-api-proxy --tailscale
+)
+
+
+

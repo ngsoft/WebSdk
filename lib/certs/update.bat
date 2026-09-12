a6bccb011aecc9faae2756ac837b442cc83b7f98
Aymeric Anger <daedelus.git@gmail.com>
2026-09-12 02:21:25 +0200

    🐛 curl cacert.pem 301

--- lib/certs/update.bat
+++ lib/certs/update.bat
@@ -5,7 +5,7 @@ pushd "%~dp0"
         move /y cacert.pem cacert.prev.pem > NUL 2>&1
     )
     del cacert.pem > NUL 2>&1
-    %~dp0../../bin/wget.exe https://curl.haxx.se/ca/cacert.pem > certs.log 2>&1
+    %~dp0../../bin/wget.exe https://curl.se/ca/cacert.pem > certs.log 2>&1
     if ERRORLEVEL 1 (
         copy /y cacert.prev.pem cacert.pem > NUL 2>&1
     )

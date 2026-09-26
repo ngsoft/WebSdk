9fddfe37599c520a0cfed94ffac02c545b04c57c
Aymeric Anger <daedelus.git@gmail.com>
2026-09-26 08:53:18 +0200

    🔧 enable repaired EnumOptions

--- var/www/adminer/config/config.php
+++ var/www/adminer/config/config.php
@@ -5,7 +5,7 @@ use Adminer\Config;
 Config::setItem('ADMINER_ACL', ['127.0', '::1']);
 Config::setItem('ADMINER_SAVEFILE', dirname(__DIR__, 4) . '/tmp/adminer.servers');
 Config::setItem('ADMINER_ENUM_FOREIGN_LIMIT', 20); // 0 => unlimited
-Config::setItem('ADMINER_ENUM_OPTIONS', false);
+Config::setItem('ADMINER_ENUM_OPTIONS', true);
 Config::setItem('ADMINER_DRIVERS', ['mysql', 'pgsql', 'sqlite', 'redis']);
 Config::setItem('ADMINER_SERVERS', [
     'MySql' => ['driver' => 'mysql', 'server' => '127.0.0.1'],
